36a484729046989ffff4d0e0aeb15e29ac533739
Felipe <75274860+luizfelipe63@users.noreply.github.com>
2023-08-07 23:45:53 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
+import { Player } from "./pages/Player";
+
+import './styles/global.css'
+
+function App() {
+
+  return (
+   <ReduxProvider store={store}>
+      <Player/>
+   </ReduxProvider>
+  )
+}
+
+export default App
